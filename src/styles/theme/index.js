e172916afdbb88dd220a42c571cc9fe8e9df59c3
juancarlos-ruiz <juancarlos.ruiz@adevinta.com>
2020-08-20 19:35:16 +0200

    Demo styles

---
+++ src/styles/theme/index.js
@@ -0,0 +1,17 @@
+import colors from './colors'
+import typography from './typography'
+import spacing from './spacing'
+import layout from './layout'
+import shadows from './shadows'
+import radius from './radius'
+
+const theme = {
+  colors,
+  typography,
+  spacing,
+  layout,
+  shadows,
+  radius
+}
+
+export default theme
---
+++ src/styles/theme/spacing.js
@@ -0,0 +1,13 @@
+import tokens from '../../../tokens/json/spacing'
+
+const spacing = {
+  xxs: tokens.spacing.xxs.value,
+  xs: tokens.spacing.xs.value,
+  sm: tokens.spacing.sm.value,
+  md: tokens.spacing.md.value,
+  lg: tokens.spacing.lg.value,
+  xl: tokens.spacing.xl.value,
+  xxl: tokens.spacing.xxl.value
+}
+
+export default spacing
